9cdaa78854f6096f3d37e572dd5e2956f3885961
GANGADHARA SHETTY PJ <GANGADHARA SHETTY PJ>
2017-10-19 20:06:25 +0530

    added place holders

--- my_codes/registrationgui.php
+++ my_codes/registrationgui.php
@@ -16,53 +16,52 @@
 <body>
 
 
-<form class="form-group">
-    <div class="container">
+<form class="form-group" style="margin:0% 0.5% 0% 0.5%;">
         <img src="cover.jpg" class="img-fluid embed-responsive bg-dark" style="padding: 5px;" >
         <fieldset action="registrationgui.php" method="post" >
-            <div  class="jumbotron bg-dark text-light " style="font-family:serif">
+            <div  class="jumbotron bg-dark text-light" style="font-family:serif">
 
 
                 <fieldset class="form-group row">
-                    <label class="col-sm-2"><h3>Team Name</h3></label>
+                    <label class="col-sm-2"><h4><b>Team Name</b></h4></label>
                     <div class="col-sm-4">
                         <input type="text" class="form-control" id="teamName" name="teamName">
                     </div>
 
                 </fieldset>
                 <fieldset class="form-group row">
-                    <label class="col-sm-5" ><h4><strong>Team Members</strong></h4></label>
+                    <label class="col-sm-5" ><h5>Team Members</h5></label>
                 </fieldset>
                 <fieldset class="form-group row">
-                    <label class="col-sm-3">Team Leader</label>
+                    <label class="col-sm-4"><h6>Team Leader</h6></label>
                 </fieldset>
                 <fieldset class="form-group row" style="margin-left: 3%">
 
                     <fieldset class="form-group row">
-                        <label class="col-sm-3">Name</label>
+                        <label class="col-sm-4">Name</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="leaderName" name="leaderName">
+                            <input style="font-family:monospace" placeholder="full name" type="text" class="form-control" id="leaderName" name="leaderName">
                         </div>
 
                     </fieldset>
                     <fieldset class="form-group  row">
-                        <label class="col-sm-3">USN</label>
+                        <label class="col-sm-4">USN</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="text" name="leaderUsn">
+                            <input style="font-family:monospace" placeholder="1SIYYCSXYZ" type="text" class="form-control" id="text" name="leaderUsn">
                         </div>
 
                     </fieldset>
                     <fieldset class="form-group  row">
-                        <label class="col-sm-3">E-mail</label>
+                        <label class="col-sm-4">E-mail</label>
                         <div class="col-sm-10">
-                            <input type="email" class="form-control" id="email" name="leaderEmail">
+                            <input style="font-family:monospace" placeholder="example@email.com" type="email" class="form-control" id="email" name="leaderEmail">
                         </div>
 
                     </fieldset>
                     <fieldset class="form-group  row">
-                        <label class="col-sm-3">Phone</label>
+                        <label class="col-sm-4">Phone</label>
                         <div class="col-sm-10">
-                            <input type="number"  class="form-control " id="phone" name="leaderPhone"  max="9999999999">
+                            <input style="font-family:monospace" placeholder="phone number" type="tel"  class="form-control " id="phone" name="leaderPhone"  maxlength="10">
                         </div>
 
                     </fieldset>
@@ -70,35 +69,36 @@
                 </fieldset>
 
                 <fieldset class="form-group row">
-                    <label class="col-sm-3">Team Member 1</label>
+                    <label class="col-sm-4"><h6>Team Member 1</h6></label>
                 </fieldset>
                 <fieldset class="form-group row" style="margin-left: 3%">
 
                     <fieldset class="form-group row">
-                        <label class="col-sm-3">Name</label>
+                        <label class="col-sm-4">Name</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="leaderName" name="member1Name">
+                            <input style="font-family:monospace" placeholder="full name" type="text" class="form-control" id="leaderName" name="member1Name">
                         </div>
 
                     </fieldset>
                     <fieldset class="form-group row">
-                        <label class="col-sm-3">USN</label>
+                        <label class="col-sm-4">USN</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" id="text" name="member1Usn">
+                            <input style="font-family:monospace" placeholder="1SIYYCSXYZ" type="text" class="form-control" id="text" name="member1Usn">
                         </div>
 
                     </fieldset>
                     <fieldset class="form-group row">
-                        <label class="col-sm-3">E-mail</label>
+                        <label class="col-sm-4">E-mail</label>
                         <div class="col-sm-10">
-                            <input type="email" class="form-control" id="email" name="member1Email">
+                            <input style="font-family:monospace" placeholder="example@email.com" type="email" class="form-control" id="email" name="member1Email">
                         </div>
 
                     </fieldset>
                     <fieldset class="form-group row">
-                        <label class="col-sm-3">Phone</label>
+                        <label class="col-sm-4">Phone</label>
                         <div class="col-sm-10">
-                            <input type="number" class="form-control " id="phone" name="member1Phone"  max="9999999999" >
+
+                            <input style="font-family:monospace" placeholder="phone number" type="tel" class="form-control"   id="phone" name="member1Phone"  maxlength="10" >
                         </div>
 
                     </fieldset>
@@ -108,7 +108,6 @@
 
             </div>
         </fieldset>
-    </div>
 </form>
 
 
